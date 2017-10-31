a1ed214ffe0eff36038f24218b4d3e47fc4f74db
Sourec <tothemyers@gmail.com>
2017-10-31 13:11:47 -0400

    Implement factoring algorithm using prime sieve

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+from math import sqrt
+
+
 def gcd_euclid(m, n):
 	"""Implementation of Euclid's algorithm for finding a GCD"""
 	while n != 0:
@@ -21,13 +24,26 @@ def gcd_consec(m, n):
 
 def sieve_to(n):
 	"""Simple sieving algorithm to get all primes up to the specified number"""
-	l = list(range(2, n))
+	l = list(range(2, n+1))
 	for n in l:
-		for i in l:
+		for i in l[l.index(n):]:
 			if i == n:
 				continue
 			if i % n == 0:
-				del l[l.index(i)]
+				del l[l.index(i)]  # This *might* run in linear time all on its own... whoops
 	return l
 
-print(sieve_to(100))
+
+def factors_of(n):
+	"""Factor a number using a prime finding sieve and a little magic"""
+	prime_list = sieve_to(int(n / 2))
+	factor_list = []
+	temp = n
+	for i in prime_list:
+		while temp % i == 0 and n != 1:
+			factor_list.append(i)
+			temp /= i
+
+	if len(factor_list) == 0:
+		factor_list.append(n)
+	return factor_list
