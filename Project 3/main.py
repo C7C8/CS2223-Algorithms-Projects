372dc9b9f9d6e036f00afb225deb2a4189c9537e
Sourec <tothemyers@gmail.com>
2017-12-06 20:01:10 -0500

    Cleanup, add docs

--- Project 3/main.py
+++ Project 3/main.py
@@ -1,17 +1,18 @@
+#!/usr/bin/python3
 import sys
 from random import randint
 from itertools import chain
 from itertools import combinations
-from timeit import timeit
+#from timeit import timeit
 
 
 def knapsack_exhaustive(ksize, items):
 	"""Conduct exhaustive search by iterating over the power set of the input item list. This is absurdly expensive"""
 	# Why do in a loop what you can do with a few lambdas?
-	powerset = chain.from_iterable(combinations(items, r) for r in range(len(items)+1))
-	powerset = filter(lambda s: sum(map(lambda i: i[0], s)) <= ksize, powerset)
-	best = max(powerset, key=lambda s: sum(map(lambda i: i[1], s)))
-	return sum(map(lambda i: i[1], best)), best
+	powerset = chain.from_iterable(combinations(items, r) for r in range(len(items)+1))	# Get all possible sets
+	powerset = filter(lambda s: sum(map(lambda i: i[0], s)) <= ksize, powerset)			# Filter to sets whose weights are less than or equal to the knapsack's size
+	best = max(powerset, key=lambda s: sum(map(lambda i: i[1], s)))						# Of those filtered sets, get the one with the highest value
+	return sum(map(lambda i: i[1], best)), list(best)									# Calculate best value and return it along with the set
 
 
 def knapsack_dp(ksize, items):
@@ -39,20 +40,16 @@ def knapsack_dp(ksize, items):
 
 
 def knapsack_own(ksize, items):
-	"""Do some recursive magic stuff to try to find a solution. I can't guarantee that this will actually work..."""
-
-	if len(items) <= 3:
-		return knapsack_exhaustive(ksize, items)
-
-	# Divide the list into two parts, sorted by weight. This is magic.
-	items = sorted(items, key=lambda x: x[0])
-	l = items[::2]
-	r = items[1::2]
-	lweight = sum(map(lambda i: i[0], l))
-	rweight = sum(map(lambda i: i[0], r))
-	lres, litems = knapsack_own((lweight / ksize) * ksize * 0.707, l)
-	rres, ritems = knapsack_own((rweight / ksize) * ksize * 0.707, r)
-	return knapsack_exhaustive(ksize, litems + ritems)
+	"""Simple ratio-based greedy algorithm. Will NOT find the optimum solution."""
+	lst = sorted(items, key=lambda i: -i[1]/i[0])  # Negative so we get a sort in *descending* order
+	size = 0
+	cur = []
+	for item in lst:
+		if size + item[0] > ksize:
+			break
+		cur.append(item)
+		size += item[0]
+	return sum(map(lambda i: i[1], cur)), cur
 
 
 def parse_input(filename):
@@ -73,10 +70,6 @@ def genList(maxItems, maxWeight, maxValue):
 	"""Generate a list of random items"""
 	return [[randint(1, maxWeight), randint(1, maxValue)] for x in range(maxItems)]
 
-def wrapper(algo, ksize, items):
-	def wrapped():
-		return algo(ksize, items)
-	return wrapped
 
 ksize = 0
 items = []
@@ -89,32 +82,40 @@ else:
 	print("Invalid number of arguments")
 	exit(132)
 
-# print("Knapsack size:\t\t%d\t\t%s" % (ksize, str(items)))
-# rVal, rLst = knapsack_exhaustive(ksize, items)
-# rWgt = sum(map(lambda i: i[0], rLst))
-# print("Exhaustive result:\t%d, %d\t%s" % (rVal, rWgt, str(rLst)))
-# rVal, rLst = knapsack_dp(ksize, items)
-# rWgt = sum(map(lambda i: i[0], rLst))
-# print("Dynamic result:\t\t%d, %d\t%s" % (rVal, rWgt, str(rLst)))
-# rVal, rLst = knapsack_own(ksize, items)
-# rWgt = sum(map(lambda i: i[0], rLst))
-# print("Own result:\t\t\t%d, %d\t%s" % (rVal, rWgt, str(rLst)))
+print("Knapsack size:\t\t%d\t\t\t\t%s" % (ksize, str(items)))
+rVal, rLst = knapsack_exhaustive(ksize, items)
+rWgt = sum(map(lambda i: i[0], rLst))
+print("Exhaustive result:\t%d, size=%d\t%s" % (rVal, rWgt, str(rLst)))
+rVal, rLst = knapsack_dp(ksize, items)
+rWgt = sum(map(lambda i: i[0], rLst))
+print("Dynamic result:\t\t%d, size=%d\t%s" % (rVal, rWgt, str(rLst)))
+rVal, rLst = knapsack_own(ksize, items)
+rWgt = sum(map(lambda i: i[0], rLst))
+print("Greedy result:\t\t%d, size=%d\t%s" % (rVal, rWgt, str(rLst)))
 
-out = [["Dynamic", "Own"]]
-# Range of items to test for...
-for itemcount in range(3, 23):
-	# Number of trials...
-	print("Testing on %d items..." % itemcount)
-	line = [0, 0]
-	for trial in range(100):
-		ksize = randint(20, 100)
-		items = genList(itemcount, 20, 100)
-		line[0] += timeit(wrapper(knapsack_dp, ksize, items), number=5)
-		line[1] += timeit(wrapper(knapsack_own, ksize, items), number=5)
-	line[0] /= 100.0
-	line[1] /= 100.0
-	out.append(line)
+# Below is some old testing code used to output the results of a series of random test to a CSV file, which was then
+# graphed. It is no longer used.
 
-with open("output.csv", "w+") as file:
-	for line in out:
-		file.write("%s, %s\n" % (line[0], line[1]))
+# def wrapper(algo, ksize, items):
+# 	def wrapped():
+# 		return algo(ksize, items)
+# 	return wrapped
+
+# out = [["Dynamic", "Own"]]
+# # Range of items to test for...
+# for itemcount in range(3, 23):
+# 	# Number of trials...
+# 	print("Testing on %d items..." % itemcount)
+# 	line = [0, 0]
+# 	for trial in range(100):
+# 		ksize = randint(20, 100)
+# 		items = genList(itemcount, 20, 100)
+# 		line[0] += timeit(wrapper(knapsack_dp, ksize, items), number=5)
+# 		line[1] += timeit(wrapper(knapsack_own, ksize, items), number=5)
+# 	line[0] /= 100.0
+# 	line[1] /= 100.0
+# 	out.append(line)
+#
+# with open("output.csv", "w+") as file:
+# 	for line in out:
+# 		file.write("%s, %s\n" % (line[0], line[1]))
